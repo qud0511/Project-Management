42f670728baac168f822a02bab0ad7385a59aaf2
JOOOOONY <qkrqudwnsqkrqudwns0511@gmail.com>
2022-10-17 17:42:14 +0900

    t3q

---
+++ t3q/img_classifi_T3Q/image_classification_train.py
@@ -0,0 +1,27 @@
+# 파일명: image_classification_train.py
+
+from image_classification_train_sub import exec_train, exec_init_svc, exec_inference
+
+import logging
+
+
+def train(tm):
+    
+    exec_train(tm)
+    logging.info('[hunmin log] the end line of the function [train]')
+
+
+def init_svc(im):
+    
+    params = exec_init_svc(im)
+    logging.info('[hunmin log] the end line of the function [init_svc]')
+    
+    return { **params }
+
+
+def inference(df, params, batch_id):
+    
+    result = exec_inference(df, params, batch_id)
+    logging.info('[hunmin log] the end line of the function [inference]')
+    
+    return { **result }
